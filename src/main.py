cfdef6fce428bb94017deffbb7d208ce6b9f8eed
Mateusz Mazur <mazurm@student.agh.edu.pl>
2023-12-06 23:32:17 +0100

    Initialized ble service

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
+from cputemp.cputemp import bt_le_server, ThermometerService, ThermometerAdvertisement
 
 def main():
-	print("Hello from kopiec!")
+
+    # @TODO: zaktualizować serwisy i advertisment cputemp
+    #
+    # Wprowadzenie do Bluetooth LE:
+    # https://devzone.nordicsemi.com/guides/short-range-guides/b/bluetooth-low-energy/posts/ble-services-a-beginners-tutorial
+    #
+    # Docelowo ten serwis (jako cały) wywoływać tylko na samym początku, dla nieskonfigurowanego urządzenia (lub po factor-resecie ???)
+
+    bt_server = bt_le_server()
+    bt_server.add_service(ThermometerService(0))
+    bt_server.register()
+
+    adv = ThermometerAdvertisement(0)
+    adv.register()
+
+    try:
+        bt_server.run()
+    except KeyboardInterrupt:
+        bt_server.quit()
+
+    #################################   
 
 
 if __name__=='__main__':
