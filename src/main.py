6fb9488dfcdd63742f43cb5e192bcd47e1b7d555
Mateusz Mazur <mazurm@student.agh.edu.pl>
2023-12-15 05:31:54 +0100

    Implemented wifi handling + iot_controller

--- src/main.py
+++ src/main.py
@@ -1,68 +1,11 @@
-from bluetooth.krecik_ble_server import KrecikBleServer
-from services.datasource import Datasource
-
-from time import sleep
-
-
-def initialize_datasource(ds):
-    print("Started initialization process.")
-
-    ble = KrecikBleServer()
-    ble.run()
-    print("Krecik BLE server running. Waiting for data...")
-
-    try:
-        awaiting_data = True
-        while awaiting_data:
-            try:
-                data_from_phone = ble.get_data()
-                print("Received data...", end=" ")
-
-                ds.load_data_from_json(
-                    data_from_phone,
-                    save=True
-                )
-                ble.set_message("S")
-                print("Data saved.")
-
-                awaiting_data = False
-            except RuntimeError as e:
-                if str(e)[1] == ':':
-                    ble.set_message(str(e)[0])
-                    print(str(e)[3:])
-                sleep(1)
-        ble.quit(delay=5)  # time to read accepted state
-
-    except KeyboardInterrupt:
-        ble.quit()
+from krecik_iot_controller.krecik_iot_controller import KrecikIOTController
 
 
 def main():
-    print("Szczesc Boze!")
-
-    ds = Datasource(
-        bt_token="krecik_krol",
-        conf_file="src/conf/datasource.json"
+    controller = KrecikIOTController()
+    controller.run(
+        sleep_interval_s = 3600
     )
-
-    if not ds.is_configured():
-        print("Data source not configured.")
-        initialize_datasource(ds)
-
-    # Od tego momentu serwis ds jest już skonfigurowany. Zawiera dane
-    # niezbędne do nawiązania połączenia z serwerem, oraz do autoryzacji
-    # na nim. Przykład:
-    print("Data source configured.")
-    print("---------------------")
-    print(ds)
-    print("---------------------")
-
-    response = request.post(
-        f"{ds.host}:{ds.port}/dupa",
-        data={"dupa": "dupa"},
-        headers={"Authorization": ds.auth_token}
-    )
-
 
 if __name__ == '__main__':
     main()
--- src/services/datasource.py
+++ src/services/datasource.py
@@ -4,8 +4,8 @@ import json
 class Datasource:
     def __init__(
             self,
-            bt_token="krecik_krol",
-            conf_file='conf/datasource.json'
+            bt_token,
+            conf_file
     ):
         self._BT_TOKEN = bt_token
         self.conf_file = conf_file
@@ -16,7 +16,6 @@ class Datasource:
         self.wifi_password = data['wifi_password']
 
         self.host = data['host']
-        self.port = data['port']
         self.user = data['user']
         self.auth_token = data['auth_token']
 
@@ -31,7 +30,6 @@ class Datasource:
         if 'wifi_ssid' not in data \
                 or 'wifi_password' not in data \
                 or 'host' not in data \
-                or 'port' not in data \
                 or 'user' not in data \
                 or 'auth_token' not in data:
             raise RuntimeError("D: Invalid data")
@@ -40,7 +38,6 @@ class Datasource:
         self.wifi_password = data['wifi_password']
 
         self.host = data['host']
-        self.port = data['port']
         self.user = data['user']
         self.auth_token = data['auth_token']
 
@@ -48,8 +45,17 @@ class Datasource:
             self.save_data_to_file()
 
     def load_data_from_file(self):
-        with open(self.conf_file, 'r') as f:
-            return json.load(f)
+        try:
+            with open(self.conf_file, 'r') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return {
+                'wifi_ssid': None,
+                'wifi_password': None,
+                'host': None,
+                'user': None,
+                'auth_token': None
+            }
 
     def save_data_to_file(self):
         with open(self.conf_file, 'w') as f:
@@ -57,7 +63,6 @@ class Datasource:
                 'wifi_ssid': self.wifi_ssid,
                 'wifi_password': self.wifi_password,
                 'host': self.host,
-                'port': self.port,
                 'user': self.user,
                 'auth_token': self.auth_token
             }
@@ -68,7 +73,6 @@ class Datasource:
         self.wifi_ssid = None
         self.wifi_password = None
         self.host = None
-        self.port = None
         self.user = None
         self.auth_token = None
         self.save_data_to_file()
@@ -77,7 +81,6 @@ class Datasource:
         return self.wifi_ssid is not None \
             and self.wifi_password is not None \
             and self.host is not None \
-            and self.port is not None \
             and self.user is not None \
             and self.auth_token is not None
 
@@ -91,6 +94,13 @@ class Datasource:
     def get_wifi_ssid(self):
         return self.wifi_ssid
 
+    def get_wifi_password(self):
+        return self.wifi_password
+
+    def set_wifi_password(self, wifi_password):
+        self.wifi_password = wifi_password
+        self.save_data_to_file()
+
     def set_wifi_ssid(self, wifi_ssid):
         self.wifi_ssid = wifi_ssid
         self.save_data_to_file()
@@ -100,13 +110,6 @@ class Datasource:
 
     def set_host(self, host):
         self.host = host
-        self.save_data_to_file()
-
-    def get_port(self):
-        return self.port
-
-    def set_port(self, port):
-        self.port = port
         self.save_data_to_file()
 
     def get_user(self):
@@ -133,6 +136,5 @@ class Datasource:
         return "wifi_ssid: " + self.__stringify_none(self.wifi_ssid) + "\n" + \
             "wifi_password: " + self.__stringify_none(self.wifi_password) + "\n" + \
             "host: " + self.__stringify_none(self.host) + "\n" + \
-            "port: " + self.__stringify_none(self.port) + "\n" + \
             "user: " + self.__stringify_none(self.user) + "\n" + \
             "auth_token: " + self.__stringify_none(self.auth_token)
