29581201d0c74a931c1ba10ac504489be17acd0e
Mateusz Mazur <mazurm@student.agh.edu.pl>
2023-12-17 18:18:24 +0100

    Added symmetric decryption for ble

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from krecik_iot_controller.krecik_iot_controller import KrecikIOTController
 
 
 def main():
-    controller = KrecikIOTController()
-    controller.run(
-        sleep_interval_s = 3600
+    controller = KrecikIOTController(
+        bt_token=b'qVGchwXL8rZoitZ6IBL4UGL-LNlDbMIMykNvuTFbnpY=',
     )
+    controller.run(
+        sleep_interval_s=3600,
+    )
+
 
 if __name__ == '__main__':
     main()
